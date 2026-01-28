b336e8b7c2fb750698da90b2c992be8bbea1103f
Tamires Correa <tamirescorrea.dev@gmail.com>
2026-01-28 17:40:32 -0300

    Initial Commit

---
+++ Cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by Tamires Corrêa on 28/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
